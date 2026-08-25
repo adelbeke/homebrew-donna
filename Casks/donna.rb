e635b4f619308ea28ce7e8363eb33bfdd7cbd445
Arthur Delbeke <arthur.delbeke.pro@gmail.com>
2026-08-25 08:51:50 +0000

    chore: update donna to v2.4.1

--- Casks/donna.rb
+++ Casks/donna.rb
@@ -1,9 +1,9 @@
 cask "donna" do
   arch arm: "arm64"
 
-  version "2.4.0"
-  sha256 arm:   "84bcc7035b3ab17e2a3b4de7cdf1dc1c3fe84049678871c3e73adf9f122bf1ab",
-         intel: "16d44b79f068a5bc5be6cac048e37328569a587d9b847e4176b5932f02300a93"
+  version "2.4.1"
+  sha256 arm:   "5ac05c6ef85bca47802d3ed265d55f770a6203dd5c79b9d2dce2e906245ca54c",
+         intel: "c53311039ff53faf79a253a9fe54fd9fdd2bee3e94c656ecf31646dab00001b6"
 
   url "https://github.com/adelbeke/donna/releases/download/v#{version}/Donna-#{version}#{"-arm64" if arch == "arm64"}.dmg"
   name "Donna"
